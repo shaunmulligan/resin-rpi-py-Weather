c03ca5b4461fe143d915ca5d1acdfffce281feed
Shaun Mulligan <switchflip.shaun@gmail.com>
2014-10-07 18:05:08 +0100

    removed the stupid non-ascii char

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ import string
  
 # Weather Icons used with the following permissions:
 #
-# VClouds Weather Icons©
+# VClouds Weather Icons
 # Created and copyrighted© by VClouds - http://vclouds.deviantart.com/
 #
 # The icons are free to use for Non-Commercial use, but If you use want to use it with your art please credit me and put a link leading back to the icons DA page - http://vclouds.deviantart.com/gallery/#/d2ynulp
